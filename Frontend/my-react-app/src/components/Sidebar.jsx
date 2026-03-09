307f869edfd4ef36dfe315e5b6df33b64d3ac8fc
pnavanetha <navaneethapemmaka@gmail.com>
2026-03-09 17:36:45 +0530

    UI changes

---
+++ Frontend/my-react-app/src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaTachometerAlt, FaDatabase, FaExchangeAlt, FaEye, FaFileAlt } from "react-icons/fa";
+import "./sidebar.css";
+
+const SideNavigation = () => {
+  return (
+    <div className="sidebar">
+
+      <h2 className="logo">Utility Bank</h2>
+
+      <ul className="menu">
+
+        <li>
+          <FaTachometerAlt />
+          <span> Dashboard</span>
+        </li>
+
+        <li className="menu-title">Masters</li>
+        <li>Customer Master</li>
+        <li>Branch Master</li>
+        <li>Account Type Master</li>
+
+        <li className="menu-title">Transactions</li>
+        <li>Customer Registration</li>
+        <li>Account Opening</li>
+        <li>Deposit</li>
+        <li>Withdrawal</li>
+        <li>Fund Transfer</li>
+
+        <li className="menu-title">Views</li>
+        <li>Customer Details</li>
+        <li>Account Summary</li>
+        <li>Transaction History</li>
+
+        <li className="menu-title">Reports</li>
+        <li>Account Statement</li>
+        <li>Daily Transactions</li>
+        <li>Customer Report</li>
+
+      </ul>
+
+    </div>
+  );
+};
+
+export default SideNavigation;
---
+++ Frontend/my-react-app/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+function Dashboard() {
+  return (
+      <div style={{ padding: "20px" }}>
+        <h1>Dashboard</h1>
+      </div>
+
+  );
+}
+
+export default Dashboard;
